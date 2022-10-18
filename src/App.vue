3b8d731ab7be357da4d436d4f3d75319b7b72afb
sergiolara92 <sergioldm92@gmail.com>
2022-10-18 12:05:45 +0200

    base-url

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
 
   <h1>TASK APP</h1>
-  
+
+  <ul>
+    <li>
+      <router-link :to="{name: 'home'}">HOME</router-link>      
+    </li>
+    <li>
+      <router-link :to="{name: 'login'}">LOGIN</router-link>
+    </li>
+  </ul>
+
+  <router-view></router-view>
+
+
 </template>
 <script setup>
+
+console.log(process.env.NODE_ENV);
+
+console.log(import.meta.env.BASE_URL);
+
 </script>
 <style scoped>
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>LOGIN</h1>
+</template>
+<script setup>
+</script>
+<style scoped>
+</style>
