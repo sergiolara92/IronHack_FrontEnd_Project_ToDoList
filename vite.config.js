0d2c159177aca3a6989178db353326a14525e656
sergiolara92 <sergioldm92@gmail.com>
2022-11-15 19:51:39 +0100

    cambio nombre

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: process.env.NODE_ENV === 'production' ? '/TaskAppProject/' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/IronHack_FrontEnd_Project_ToDoList/' : '/',
     build: {
         outDir: 'docs',
         rollupOptions: {
