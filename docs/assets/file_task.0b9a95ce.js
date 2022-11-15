93a8fa9006cfc22b472a6f377975dfe0a66c3989
sergiolara92 <sergioldm92@gmail.com>
2022-11-15 19:52:20 +0100

    nombre

---
+++ docs/assets/file_task.0b9a95ce.js
@@ -0,0 +1,1 @@
+import{m as t}from"./file_index.352fb304.js";const k=t("task",{state:()=>({tasks:[]}),actions:{setTask(){},updateTask(s,a){},deleteTask(s){},addTask(s){}}});export{k as u};
--- docs/assets/file_task.5491028c.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./file_index.6383c89d.js";const k=t("task",{state:()=>({tasks:[]}),actions:{setTask(){},updateTask(s,a){},deleteTask(s){},addTask(s){}}});export{k as u};
